01a209ef7f7f94a6ea0c3b5eeab11bff3960fb64
soumya-99 <soumyadeepmondal1234@gmail.com>
2025-04-12 17:18:06 +0530

    Deno with redis starter

---
+++ client.ts
@@ -0,0 +1,5 @@
+import { Redis } from "npm:ioredis";
+
+const RedisClient = new Redis()
+
+export default RedisClient;
---
+++ main.ts
@@ -0,0 +1,39 @@
+import RedisClient from "./client.ts";
+
+Deno.serve({
+  port: 8000,
+}, async (req) => {
+  const url = new URL(req.url);
+  const { pathname } = url;
+
+  if (pathname === "/") {
+    return new Response("Hello World");
+  }
+
+  if (pathname === "/redis") {
+    const cache = await RedisClient.get("jsonplaceholder");
+    if (cache) {
+      console.log("Cache hit");
+      return new Response(cache);
+    }
+
+    console.log("Cache miss");
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    await RedisClient.set("jsonplaceholder", JSON.stringify(data));
+    await RedisClient.expire("jsonplaceholder", 30);
+
+    return new Response(JSON.stringify(data));
+  }
+
+  return new Response("Not Found", { status: 404 });
+}
+);
